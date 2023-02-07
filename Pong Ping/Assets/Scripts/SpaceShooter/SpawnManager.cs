b113fd8b2d38c743fbd0dd14f3b00311ce2b8113
Mauro Moreno <mmauromoreno1@gmail.com>
2023-02-07 13:26:51 -0300

    [SPACE SHOOTER] - Created the enemy spawn manager

--- Pong Ping/Assets/Scripts/SpaceShooter/SpawnManager.cs
+++ Pong Ping/Assets/Scripts/SpaceShooter/SpawnManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject buffPrefab;
+
+    private float leftBorder = -2.3f;
+    private float rightBorder = 2.3f;
+
+    private void OnEnable()
     {
-        
+        InvokeRepeating(nameof(SpawnEnemy), Random.Range(0.5f, 2f), Random.Range(0.5f, 2f));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        CancelInvoke(nameof(SpawnEnemy));
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject enemies = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        enemies.transform.position += Vector3.down * Random.Range(leftBorder, rightBorder);
     }
 }
